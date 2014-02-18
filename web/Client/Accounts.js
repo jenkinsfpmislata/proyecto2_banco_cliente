b8683f4df854e082801d09b9bc8e63c52a3bf9e6
JCVarea <alumno@2A5PC22>
2014-02-18 11:10:31 +0100

    ultimo  srpint, probando sacar las cuentas de un cliente servidor echo, probando el .js

--- web/Client/Accounts.js
+++ web/Client/Accounts.js
@@ -6,7 +6,7 @@ app.controller("AccountsCtrl",function($scope, $http, $routeParams){
 
     $scope.readAccountClient = function() {
         $http.get("/proyecto2_bank_server/api/"+ $scope.params.idCliente+"/CuentaBancaria/").success(function(result) {
-            $scope.cuentaBancaria = result;
+            $scope.cuentasBancarias = result;
         });
     };
 });
