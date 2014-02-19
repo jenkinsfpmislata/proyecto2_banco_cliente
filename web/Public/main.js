29ffdf00fc218e0c215788a4b3fcb30beb33aa1f
JCVarea <alumno@2A5PC22>
2014-02-19 11:32:49 +0100

    Intentando un Login, prueba 2

--- web/Public/main.js
+++ web/Public/main.js
@@ -6,7 +6,7 @@ app.controller("mainCtrl",function($scope, $http){
 
 $scope.logearse = function() {
 alert("funciona");
-        $http.post("/proyecto2_bank_server/api/Credenciales/", $scope.nickLogin).success(function(result) {
+        $http.post("/proyecto2_bank_server/api/Cliente/", $scope.nickLogin).success(function(result) {
             alert("funciona2");
         });
         $location.path("/../Client/clientIndex.html");
