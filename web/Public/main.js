e861154eb305c96d965a5889dd82426f63e83735
JCVarea <alumno@2A5PC22>
2014-02-20 09:21:22 +0100

    Intentando un Login, lo he dejado como estaba, pero en el main.js e creado una funcion Logearse que no funciona porque no se como se pone la url para llamar al servidor prueba 2

--- web/Public/main.js
+++ web/Public/main.js
@@ -1,12 +1,11 @@
 app.controller("mainCtrl",function($scope, $http){
      
+    $scope.credenciales = {};
+  
     
-    $scope.nickLogin = null;
-   
-
-$scope.logearse = function() {
+ $scope.logearse = function() {
 alert("funciona");
-        $http.post("/proyecto2_bank_server/api/Cliente/", $scope.nickLogin).success(function(result) {
+        $http.post("/proyecto2_bank_server/api/Cliente/", $scope.credenciales).success(function(result) {
             alert("funciona2");
         });
         $location.path("/../Client/clientIndex.html");
