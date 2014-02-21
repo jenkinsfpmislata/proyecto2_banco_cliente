bcc1c09d1253651f374a7233a150094a9689dae9
JCVarea <alumno@2A5PC22>
2014-02-21 12:05:29 +0100

    Intentando un Login, login lee el "login" y debuelve el cliente

--- web/Client/Accounts.js
+++ web/Client/Accounts.js
@@ -9,7 +9,7 @@ app.controller("AccountsCtrl",function($scope, $http, $routeParams){
      $scope.readClient=function(){
          $http.get("/proyecto2_bank_server/api/Login").success(function(result) {
          $scope.cliente = result;
-       alert($scope.cliente.idCliente);
+      
      })
      }
      $scope.readClient();
@@ -18,6 +18,7 @@ app.controller("AccountsCtrl",function($scope, $http, $routeParams){
         
         $http.get("/proyecto2_bank_server/api/"+ $scope.cliente.idCliente +"/CuentaBancaria").success(function(result) {
             $scope.cuentasBancarias = result;
+             alert($scope.cliente.idCliente);
         });
     };
      $scope.readAccountClient();
